3cbf12ea69626a41d07e8149d2c3b066a31804d4
Saliou <farydioufsaliou@gmail.com>
2021-10-28 11:01:23 +0200

    Initialisationn du projet

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class GildedRoseTest {
+
+  @Test
+  @DisplayName("Test that the name is unchanged")
+  void testName() {
+    Item element = new Item("foo", 0, 0);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality()
+    assertThat(element.name, is("foo"));
+  }
+
+}
