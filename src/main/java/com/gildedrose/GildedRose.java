8d3d7363d194b5cecf37697feee174dc9b10b70d
Saliou <farydioufsaliou@gmail.com>
2021-10-29 17:30:46 +0200

    Refactoring: Adding other extract method (qualityDecreased, sellInDecreased and qualityNull

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,15 +15,33 @@ class GildedRose {
             item.quality = item.quality + 1;
         }
     }
+
+    private void qualityDecreased(Item item)
+    {
+        if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality = item.quality - 1;
+            }
+        }
+    }
+
+    private void sellInDecreased(Item item)
+    {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void qualityNull(Item item)
+    {
+        item.quality = 0;
+    }
+
     private void updateItem(Item item)
     {
         if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
+            qualityDecreased(item);
         } else {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -40,20 +58,14 @@ class GildedRose {
             }
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+        sellInDecreased(item);
 
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
+                    qualityDecreased(item);
                 } else {
-                    item.quality = 0;
+                    qualityNull(item);
                 }
             } else {
                 qualityIncreased(item);
