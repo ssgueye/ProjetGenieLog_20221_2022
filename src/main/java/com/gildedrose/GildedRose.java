cc57bb9700871d8c22da38481e78d9b34b3c672b
Saliou <farydioufsaliou@gmail.com>
2021-10-30 01:48:56 +0200

    Refactoring: Put some constants to make some values more comprehensive

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,31 +1,36 @@
 package com.gildedrose;
 
 class GildedRose {
+
     Item[] items;
+    private final int TenDaysBeforeExpired = 10;
+    private final int FiveDaysBeforeExpired = 5;
+    private final int QualityMaxValue = 49;
 
-    public GildedRose(Item[] items) {
-
+    public GildedRose(Item[] items)
+    {
         this.items = items;
     }
 
     private void qualityIncreased(Item item)
     {
-        if (item.quality < 50)
+        if (item.quality <= QualityMaxValue)
         {
-            item.quality = item.quality + 1;
+            item.quality++;
         }
     }
 
     private void qualityDecreased(Item item)
     {
-        if (item.quality > 0) {
-              item.quality = item.quality - 1;
+        if (item.quality > 0)
+        {
+              item.quality--;
         }
     }
 
     private void sellInDecreased(Item item)
     {
-         item.sellIn = item.sellIn - 1;
+         item.sellIn--;
     }
 
     private void qualityNull(Item item)
@@ -35,12 +40,12 @@ class GildedRose {
 
     private void nearToExpired(Item item)
     {
-        if(item.sellIn <= 10)
+        if(item.sellIn <= TenDaysBeforeExpired)
         {
             qualityIncreased(item);
         }
 
-        if(item.sellIn <= 5)
+        if(item.sellIn <= FiveDaysBeforeExpired)
         {
             qualityIncreased(item);
         }
@@ -48,50 +53,48 @@ class GildedRose {
 
     private void updateItem(Item item)
     {
-
-        if(item.name.equals("Aged Brie"))
+        switch(item.name)
         {
-            qualityIncreased(item);
-            sellInDecreased(item);
-
-            if(item.sellIn < 0)
-            {
+            case "Aged Brie":
                 qualityIncreased(item);
-            }
-        }
+                sellInDecreased(item);
 
-        else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
-        {
-            qualityIncreased(item);
-            nearToExpired(item);
-            sellInDecreased(item);
-            if(item.sellIn < 0)
-            {
-                qualityNull(item);
-            }
+                if(item.sellIn < 0)
+                {
+                    qualityIncreased(item);
+                }
+                break;
 
-        }
+            case "Backstage passes to a TAFKAL80ETC concert":
+                qualityIncreased(item);
+                nearToExpired(item);
+                sellInDecreased(item);
 
-        else if(item.name.equals("Sulfuras, Hand of Ragnaros"))
-        {
-            //Nothing to do
-        }
+                if(item.sellIn < 0)
+                {
+                    qualityNull(item);
+                }
+                break;
 
-        else
-        {
-            qualityDecreased(item);
-            sellInDecreased(item);
-            if(item.sellIn < 0)
-            {
+            case "Sulfuras, Hand of Ragnaros":
+                //Nothing to Do
+                break;
+
+            default:
                 qualityDecreased(item);
-            }
+                sellInDecreased(item);
+
+                if(item.sellIn < 0)
+                {
+                    qualityDecreased(item);
+                }
         }
     }
 
-
     public void updateQuality()
     {
-        for (Item item : items) {
+        for (Item item : items)
+        {
             updateItem(item);
         }
     }
